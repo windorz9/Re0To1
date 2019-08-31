a749b619b1d7c842c3967b8d53dbde16ff766939
windorz9 <1925449823@qq.com>
2019-08-31 09:51:03 +0800

    添加开屏展示逻辑

---
+++ Re0To1/Re0To1/Class/ZXSplashView.h
@@ -0,0 +1,17 @@
+//
+//  ZXSplashView.h
+//  Re0To1
+//
+//  Created by windorz on 2019/8/30.
+//  Copyright © 2019 Q. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ 自定义闪屏
+ */
+@interface ZXSplashView : UIImageView
+
+@end
+
---
+++ Re0To1/Re0To1/Class/ZXSplashView.m
@@ -0,0 +1,39 @@
+//
+//  ZXSplashView.m
+//  Re0To1
+//
+//  Created by windorz on 2019/8/30.
+//  Copyright © 2019 Q. All rights reserved.
+//
+
+#import "ZXSplashView.h"
+#import "ZXScreen.h"
+
+@implementation ZXSplashView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    if (self) {
+        // 为自身设置图片 并添加跳过按钮
+        self.image = [UIImage imageNamed:@"icon.bundle/splashImage.jpg"];
+        self.userInteractionEnabled = YES;
+        
+        UIButton *skipBtn = [[UIButton alloc] initWithFrame:UIRect(300, 100, 60, 40)];
+        [self addSubview:skipBtn];
+        skipBtn.backgroundColor = [UIColor lightGrayColor];
+        [skipBtn setTitle:@"跳 过" forState:UIControlStateNormal];
+        [skipBtn addTarget:self action:@selector(_clickSkipBtn) forControlEvents:UIControlEventTouchUpInside];
+        
+    }
+    return self;
+    
+}
+
+#pragma mark Private Method
+- (void)_clickSkipBtn {
+    [self removeFromSuperview];
+}
+
+
+@end
